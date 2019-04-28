0570caccfb4557f9f4b7defea8fae6273ddadb0c
Ivan <ivanmy3007@gmail.com>
2019-04-28 22:00:18 +0300

    test commit

--- WebBjola/Controllers/HomeController.cs
+++ WebBjola/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace WebBjola.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page.1";
 
             return View();
         }
